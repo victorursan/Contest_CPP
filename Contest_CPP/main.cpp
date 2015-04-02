f4491c1d0c3bb4654f9c8aaf0976434b8e927521
Victor Ursan <victor.ursan@gmail.com>
2015-04-02 11:29:55 +0300

    Initial Commit

---
+++ Contest_CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Contest_CPP
+//
+//  Created by Victor Ursan on 4/2/15.
+//  Copyright (c) 2015 Victor Ursan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
